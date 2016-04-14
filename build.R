e4b697858b11c8a7ae89e2de102d4265446159ea
Sego, Landon H <landon.sego@pnnl.gov>
2016-04-13 19:46:16 -0600

    First pass of documentation complete

--- build.R
+++ build.R
@@ -10,7 +10,7 @@ knitr::opts_knit$set(root.dir = normalizePath("."))
 packagedocs::render_docs(
   code_path = "~/Work/github/glmnetLRC",
   docs_path = ".",             # location of docs directory
-  package_name = "glmnetLRC",      # name of the package
+  package_name = "glmnetLRC",  # name of the package
   main_toc_collapse = TRUE,    # use collapsing toc on main page
   rd_toc_collapse = TRUE,      # use collapsing toc on rd page
   lib_dir = "assets",          # put assets in "assets" directory
@@ -37,68 +37,49 @@ if(!require("rsed")) {
 ##                  inFile = "index.html", outFile = "index.html")
 
 # Manual edits of rd.html
-rsed::streamEdit(list(# Fix link for sorting a dataframe
-                      ## s = list(pattern = "= #https://stat.ethz.ch/pipermail/r-help/2004-september/057954>",
-                      ##          replacement = "= https://stat.ethz.ch/pipermail/r-help/2004-September/057954.html>",
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix left link for dbb
-                      ## s = list(pattern = '<li><a href="#dbb">dbb</a></li>',
-                      ##          replacement = '<li><a href="#dbb">dbb, pbb, qbb, rbb</a></li>',
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix header text for dbb
-                      ## r = list(at = "<h2>dbb</h2>",
-                      ##          replacement = "<h2>dbb, pbb, qbb, rbb</h2>",
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix left link for dkbinom
-                      ## s = list(pattern = '<li><a href="#dkbinom">dkbinom</a></li>',
-                      ##          replacement = '<li><a href="#dkbinom">dkbinom, pkbinom</a></li>',
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix header text for dkbinom
-                      ## r = list(at = "<h2>dkbinom</h2>",
-                      ##          replacement = "<h2>dkbinom, pkbinom</h2>",
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix the methods for hpd
-                      ## s = list(pattern = "printhpd(x", replacement = "print(x", fixed = TRUE),
-                      ## s = list(pattern = "plothpd(x", replacement = "plot(x", fixed = TRUE),
+rsed::streamEdit(list(# Fix print method for 'lossMat'
+                      r = list(at = "printlossMat(x, ...)</code></pre>",
+                               replacement = "## S3 method for class 'lossMat'\nprint(x, ...)</code></pre>",
+                               fixed = TRUE),
 
-                      ## # Fix the link for grabLast in getExtension
-                      ## s = list(pattern = "<code><a href='grabLast.html'>",
-                      ##          replacement = "<code><a href='#grablast'>",
-                      ##          fixed = TRUE),
+                      # Fix print method for 'glmnetLRC'
+                      r = list(at = "printglmnetLRC(x, verbose = TRUE, ...)",
+                               replacement = "## S3 method for class 'glmnetLRC'\nprint(x, verbose = TRUE, ...)",
+                               fixed = TRUE),
                       
-                      ## # Fix the link for plapply in dfplapply
-                      ## s = list(pattern = "<code><a href='plapply.html'>",
-                      ##          replacement = "<code><a href='#plapply'>",
-                      ##          fixed = TRUE),
+                      # Fix plot method for 'glmnetLRC'
+                      r = list(at = "plotglmnetLRC(x, ...)",
+                               replacement = "## S3 method for class 'glmnetLRC'\nplot(x, ...)",
+                               fixed = TRUE),
+
+                      # Fix coef method for 'glmnetLRC'
+                      r = list(at = "coefglmnetLRC(object, ...)",
+                               replacement = "## S3 method for class 'glmnetLRC'\ncoef(object, ...)",
+                               fixed = TRUE),
+
+                      # Fix predict method for 'glmnetLRC'
+                      r = list(at = "predictglmnetLRC(object, newdata, truthCol = NULL,",
+                               replacement = "## S3 method for class 'glmnetLRC'\npredict(object, newdata, truthCol = NULL,",
+                               fixed = TRUE),
+
+                      # Fix extract method for 'glmnetLRC'
+                      r = list(at = "extractglmnetLRC(object, ...)</code></pre>",
+                               replacement = "## S3 method for class 'glmnetLRC'\nextract(object, ...)</code></pre>",
+                               fixed = TRUE),
+
+                      # Fix summary method for 'LRCpred'
+                      r = list(at = "summaryLRCpred(object, ...)</code></pre>",
+                               replacement = "<pre class=\"r\"><code>## S3 method for class 'LRCpred'\nsummary(object, ...)</code></pre>",
+                               fixed = TRUE),
                       
-                      ## # Fix the link for smartFilter in movAvg2
-                      ## s = list(pattern = "<code><a href='smartFilter.html'>",
-                      ##          replacement = "<code><a href='#smartfilter'>",
-                      ##          fixed = TRUE),
+                      # Fix glmnetLRC link in glmnetLRC_fit
+                      s = list(pattern = "<code><a href='glmnetLRC.html'>",
+                               replacement = "<code><a href='#glmnetlrc'>",
+                               fixed = TRUE),
                       
-                      ## # Fix the link for smartTimeAxis in timeDiff.eg
-                      ## s = list(pattern = "<code><a href='smartTimeAxis.html'>",
-                      ##          replacement = "<code><a href='#smarttimeaxis'>",
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix the link for timeIntegration in PowerData
-                      ## s = list(pattern = "<code><a href='timeIntegration.html'>",
-                      ##          replacement = "<code><a href='#timeintegration'>",
-                      ##          fixed = TRUE),
-                      
-                      ## # Fix the link for timeDiff in timeData
-                      ## s = list(pattern = "<code><a href='timeDiff.html'>",
-                      ##          replacement = "<code><a href='#timediff'>",
-                      ##          fixed = TRUE),
-                 
                       # Fix the author field
                       r = list(at = "<strong>Authors:</strong> (none)",
-                               replacement = "<strong>Author:</strong> Landon Sego",
+                               replacement = "<strong>Authors:</strong> Landon H. Sego, Alexander M. Venzin, John A. Ramey",
                                fixed = TRUE)),
                  
                  inFile = "rd.html", outFile = "rd.html")
